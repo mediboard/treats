31c7e51b13e2a8cde4ed876086f4e7b8cb19af92
Paul Dittamo <pvdittamo@gmail.com>
2022-07-24 19:06:22 -0700

    add groups workflow

---
+++ data_pipelines/workflows/groups_workflow.py
@@ -0,0 +1,40 @@
+"""writes groups table"""
+import os
+import pickle
+import pandas as pd
+
+# TODO update this
+STUDIES_PICKLE_FILE_PATH = "/Users/pauldittamo/src/mediboard/"
+
+
+def create_groups_table() -> pd.DataFrame:
+    with open(STUDIES_PICKLE_FILE_PATH + 'adjusted_int_admins.pkl', 'rb') as f:
+        adjusted_int_admins = pickle.load(f)
+
+    groups_table = adjusted_int_admins[
+        ['study_id', 'group_id', 'title', 'description']].drop_duplicates().reset_index(drop=True)
+    groups_table = groups_table.rename_axis(['id'], axis=0)
+
+    return groups_table
+
+
+def clean_groups_table(groups_table: pd.DataFrame) -> pd.DataFrame:
+    groups_table = groups_table.rename(columns={
+        'study_id': 'study',
+        'group_id': 'study_id',
+    })[['title', 'study_id', 'description', 'study']]
+    return groups_table
+
+
+# requires studies_workflow + admins workflow
+def groups_workflow() -> None:
+    pre_cleaned_groups_table = create_groups_table()
+    groups_table = clean_groups_table(pre_cleaned_groups_table)
+
+    print(groups_table)
+    print(groups_table.keys())
+    print(groups_table.iloc[1])
+
+
+if __name__ == "__main__":
+    groups_workflow()
