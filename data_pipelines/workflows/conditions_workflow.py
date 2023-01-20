36ad4ea1158c91b598629b6bff0a447d66620017
Davon Prewitt <davonprewitt@gmail.com>
2023-01-20 01:54:26 -0700

    Lint

--- data_pipelines/workflows/conditions_workflow.py
+++ data_pipelines/workflows/conditions_workflow.py
@@ -6,23 +6,31 @@ import re
 
 from sqlalchemy import create_engine
 
-DATA_PATH = os.environ.get('DATA_PATH', default="/Users/davonprewitt/data")
-DATABASE_URL = os.environ.get('DATABASE_URL', default="postgresql://davonprewitt@localhost:5432")
+DATA_PATH = os.environ.get("DATA_PATH", default="/Users/davonprewitt/data")
+DATABASE_URL = os.environ.get(
+    "DATABASE_URL", default="postgresql://davonprewitt@localhost:5432"
+)
 
 
 def load_pre_cleaned_studies_table() -> pd.DataFrame:
-    with open(DATA_PATH + '/pre_cleaned_studies_table.pkl', 'rb') as f:
+    with open(DATA_PATH + "/pre_cleaned_studies_table.pkl", "rb") as f:
         studies_table = pickle.load(f)
     return studies_table
 
 
 def get_conditions(studies_table: pd.DataFrame) -> pd.DataFrame:
-    conditions = studies_table.explode('conditions')[['conditions', 'study_id']]
-    conditions['alpha_num'] = conditions['conditions'].apply(lambda x: re.sub(r'[^a-zA-Z0-9 ]', '', x))
-    conditions['sorted'] = conditions['alpha_num'].str.split().apply(sorted).apply(tuple)
-    conditions_dict = {k: i for i, k in enumerate(conditions['sorted'].unique())}
-    conditions['condition_id'] = conditions['sorted'].apply(lambda x: conditions_dict[x])
-    conditions['l_conditions'] = conditions['conditions'].apply(lambda x: [x])
+    conditions = studies_table.explode("conditions")[["conditions", "study_id"]]
+    conditions["alpha_num"] = conditions["conditions"].apply(
+        lambda x: re.sub(r"[^a-zA-Z0-9 ]", "", x)
+    )
+    conditions["sorted"] = (
+        conditions["alpha_num"].str.split().apply(sorted).apply(tuple)
+    )
+    conditions_dict = {k: i for i, k in enumerate(conditions["sorted"].unique())}
+    conditions["condition_id"] = conditions["sorted"].apply(
+        lambda x: conditions_dict[x]
+    )
+    conditions["l_conditions"] = conditions["conditions"].apply(lambda x: [x])
     return conditions
 
 
@@ -31,31 +39,34 @@ def most_common(lst):
 
 
 def create_conditions_table(conditions: pd.DataFrame) -> pd.DataFrame:
-    conditions_table = pd.DataFrame(conditions.groupby('condition_id')['l_conditions'].apply(sum).apply(most_common))
-    conditions_table = conditions_table.set_axis(['name'], axis=1, inplace=False)
-    conditions_table = conditions_table.rename_axis(['id'], axis=0)
+    conditions_table = pd.DataFrame(
+        conditions.groupby("condition_id")["l_conditions"].apply(sum).apply(most_common)
+    )
+    conditions_table = conditions_table.set_axis(["name"], axis=1, inplace=False)
+    conditions_table = conditions_table.rename_axis(["id"], axis=0)
     return conditions_table
 
 
 def create_study_conditions_table(conditions: pd.DataFrame) -> pd.DataFrame:
-    study_conditions = conditions[['study_id', 'condition_id']].rename(columns={
-        'study_id': 'study',
-        'condition_id': 'condition'
-    }).reset_index(drop=True)
+    study_conditions = (
+        conditions[["study_id", "condition_id"]]
+        .rename(columns={"study_id": "study", "condition_id": "condition"})
+        .reset_index(drop=True)
+    )
     return study_conditions
 
 
 def add_study_id(table: pd.DataFrame) -> pd.DataFrame:
     db = create_engine(DATABASE_URL)
-    study_ids = pd.read_sql('select id, nct_id from studies', db.connect())
-    merged_table = table.merge(study_ids, left_on='study', right_on='nct_id')
+    study_ids = pd.read_sql("select id, nct_id from studies", db.connect())
+    merged_table = table.merge(study_ids, left_on="study", right_on="nct_id")
 
-    return merged_table[['id','study', 'condition']]
+    return merged_table[["id", "study", "condition"]]
 
 
 def upload_to_db(table_name: str, table: pd.DataFrame):
     db = create_engine(DATABASE_URL)
-    table.to_sql(str, db, index=False, if_exists='append')
+    table.to_sql(str, db, index=False, if_exists="append")
 
 
 # requires studies_workflow to write pre_cleaned studies_table to disk
@@ -64,11 +75,11 @@ def conditions_workflow() -> None:
 
     conditions = get_conditions(studies_table=studies_table)
     conditions_table = create_conditions_table(conditions=conditions)
-    upload_to_db('conditions', conditions_table)
+    upload_to_db("conditions", conditions_table)
 
     study_conditions_table = create_study_conditions_table(conditions=conditions)
     study_conditions_table = add_study_id(study_conditions_table)
-    upload_to_db('study_conditions', study_conditions_table)
+    upload_to_db("study_conditions", study_conditions_table)
 
     print(conditions_table)
     print(conditions_table.keys())
@@ -81,4 +92,3 @@ def conditions_workflow() -> None:
 
 if __name__ == "__main__":
     conditions_workflow()
-
